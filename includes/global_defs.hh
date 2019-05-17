56e69ea930935c384b10189aa6a7bd8f530bb4e0
Christian Reisinger <reisinger@itp.uni-frankfurt.de>
2019-05-17 16:13:47 +0200

    Added geometry2.hh, global_defs.hh. Implemented APE smearing.

---
+++ includes/global_defs.hh
@@ -0,0 +1,16 @@
+/*
+ * global_defs.hh
+ *
+ *  Created on: 07.03.2019
+ *      Author: reisinger
+ */
+
+#ifndef INCLUDE_GLOBAL_DEFS_HH_
+#define INCLUDE_GLOBAL_DEFS_HH_
+
+
+constexpr int SUN_N = 2;
+constexpr int SUN_elems = 2 * SUN_N * SUN_N;
+constexpr int SUN_dim = SUN_N * SUN_N - 1;
+
+#endif /* INCLUDE_GLOBAL_DEFS_HH_ */
