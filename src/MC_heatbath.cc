c3b18c1cd5fd789101ff8ac3eb76c8e5dfcc6840
Christian Reisinger <reisinger@itp.uni-frankfurt.de>
2019-08-14 14:25:05 +0200

    Moved the update algorithm to heatbath.hh/.cc
    * updated Makefile

--- src/MC_heatbath.cc
+++ src/MC_heatbath.cc
@@ -20,13 +20,9 @@
 #include <linear_algebra.hh>
 #include <ranlux.hh>
 #include <Wilson_loops.hh>
+#include <heatbath.hh>
 
 #include <helper_functions.hh>
-
-// lattice extensions
-int T, L;
-
-double *gauge_field;
 
 void handle_GNU_options(int argc, char **&argv, std::vector<int> &bound_ts, std::string &prefix) {
 
@@ -38,11 +34,11 @@ void handle_GNU_options(int argc, char **&argv, std::vector<int> &bound_ts, std:
 	int opt = -1, long_opts_i = 0;
 	while ((opt = getopt_long(argc, argv, "f:p:", long_opts, &long_opts_i)) != -1) {
 		switch (opt) {
-		case 'f':
-			bound_ts = parse_unsigned_int_list(optarg);
+			case 'f':
+				bound_ts = parse_unsigned_int_list(optarg);
 			break;
-		case 'p':
-			prefix = optarg;
+			case 'p':
+				prefix = optarg;
 			break;
 		}
 	}
@@ -53,7 +49,7 @@ void handle_GNU_options(int argc, char **&argv, std::vector<int> &bound_ts, std:
 int main(int argc, char **argv) {
 	using namespace std;
 
-	if (argc < 9 ||  argc > 14) {
+	if (argc < 9 || argc > 14) {
 		cout << "Usage: " << argv[0]
 				<< " [(--fixed-bounds | -f) <ts>] [(--prefix | -p) <prfx>] <output_dir> <beta> <T> <L> <seed> (cold | hot | <init_file>) <num_MC_sweeps_max> <num_MC_sweeps_out>\n";
 		return 0;
@@ -65,7 +61,7 @@ int main(int argc, char **argv) {
 
 	string path(argv[1]);
 	double beta;
-	int seed, num_MC_sweeps_max, num_MC_sweeps_out;
+	int T, L, seed, num_MC_sweeps_max, num_MC_sweeps_out;
 
 	stringstream args_ss;
 	args_ss << argv[2] << " " << argv[3] << " " << argv[4] << " " << argv[5] << " " << argv[7] << " " << argv[8];
@@ -95,6 +91,7 @@ int main(int argc, char **argv) {
 
 	InitializeRand(seed);
 
+	double *gauge_field;
 	Gauge_Field_Alloc(&gauge_field, T, L);
 
 	if (string(argv[6]) == string("hot")) {
@@ -122,128 +119,7 @@ int main(int argc, char **argv) {
 	}
 
 	for (int MC_sweep_i = 1; MC_sweep_i <= num_MC_sweeps_max; MC_sweep_i++) {
-		for (int it = 0; it < T; it++) {
-			bool on_boundary = contains(bound_ts, it);
-			for (int ix = 0; ix < L; ix++) {
-				for (int iy = 0; iy < L; iy++) {
-					for (int iz = 0; iz < L; iz++) {
-						for (int mu = 0; mu < 4; mu++) {
-
-							if (on_boundary && mu != 0)
-								continue;
-
-// Cf. Montvay/Münster, sec. 7.3.1, page 399.
-
-// S_l = "sum over staples", more precisely S = -\beta/N Re(Tr(U_l S_l)).
-
-							double S_l[8];
-							cm_eq_zero(S_l);
-
-							double U1[8], U2[8];
-							for (int nu = 0; nu < 4; nu++) {
-								if (nu == mu)
-									continue;
-
-								int n[4] = { it, ix, iy, iz };
-								int index1, index2, index3;
-
-								// negative mu-nu staple
-								n[nu] -= 1;
-								index1 = ggi(get_index(n[0], n[1], n[2], n[3], T, L), nu);
-								index2 = ggi(get_index(n[0], n[1], n[2], n[3], T, L), mu);
-								n[mu] += 1;
-								index3 = ggi(get_index(n[0], n[1], n[2], n[3], T, L), nu);
-								cm_eq_cm_ti_cm(U1,
-										gauge_field + index2,
-										gauge_field + index3);
-								cm_eq_cm_dag_ti_cm(U2, gauge_field + index1, U1);
-								cm_pl_eq_cm(S_l, U2);
-
-								// positive mu-nu staple
-								n[0] = it;
-								n[1] = ix;
-								n[2] = iy;
-								n[3] = iz;
-								index1 = ggi(get_index(n[0], n[1], n[2], n[3], T, L), nu);
-								n[nu] += 1;
-								index2 = ggi(get_index(n[0], n[1], n[2], n[3], T, L), mu);
-								n[mu] += 1;
-								n[nu] -= 1;
-								index3 = ggi(get_index(n[0], n[1], n[2], n[3], T, L), nu);
-								cm_eq_cm_ti_cm_dag(U1,
-										gauge_field + index2,
-										gauge_field + index3);
-								cm_eq_cm_ti_cm(U2, gauge_field + index1, U1);
-								cm_pl_eq_cm(S_l, U2);
-							}
-
-							cm_dag_eq_cm(S_l);
-
-// Cf. eq. (7.106).
-// Step i) and ii) on page 401.
-							double k = sqrt(S_l[0] * S_l[6] - S_l[1] * S_l[7] - S_l[2] * S_l[4] + S_l[3] * S_l[5]);
-							double beta_k = beta * k;
-
-							double y_min = exp(-beta_k);
-							double y_max = exp(+beta_k);
-							double y;
-
-							double a[4];
-							while (1) {
-								y = y_min + (y_max - y_min) * DRand();
-								a[0] = log(y) / beta_k;
-								if (DRand() <= sqrt(1.0 - pow(a[0], 2.0)))
-									break;
-							}
-
-// Step iii) on page 401.
-							double norm;
-							while (1) {
-								a[1] = 2.0 * DRand() - 1.0;
-								a[2] = 2.0 * DRand() - 1.0;
-								a[3] = 2.0 * DRand() - 1.0;
-								norm = pow(a[1], 2.0) + pow(a[2], 2.0) + pow(a[3], 2.0);
-								if (norm >= 0.0000000001 && norm <= 1.0)
-									break;
-							}
-
-							norm = sqrt((1.0 - pow(a[0], 2.0)) / norm);
-							a[1] *= norm;
-							a[2] *= norm;
-							a[3] *= norm;
-
-// Step iv) on page 401.
-// U_0 = k S_l^{-1} = S_l^\dagger / k   (cf. eq. (7.105), (7.110))
-							double U_0[8];
-							cm_eq_cm_dag(U_0, S_l);
-							cm_ti_eq_re(U_0, 1.0 / k);
-
-// U_0l = a_0 + i \sigma_j a_j   (cf. eq. (7.110))
-							double U_0l[8];
-							cm_from_h(U_0l, a);
-							cm_eq_cm_ti_cm(U1, U_0l, U_0);
-
-// Project the link to SU(2) (roundoff errors accumulate during simulation).
-							double h[4];
-							h_from_cm(h, U1);
-							norm = 1.0 / sqrt(pow(h[0], 2.0) + pow(h[1], 2.0) + pow(h[2], 2.0) + pow(h[3], 2.0));
-							if (abs(norm - 1.0) > pow(10.0, -12.0)) {
-								cerr << "Error: New link is not in SU(2).\n";
-								return 1;
-							}
-							h[0] *= norm;
-							h[1] *= norm;
-							h[2] *= norm;
-							h[3] *= norm;
-							cm_from_h(U1, h);
-
-							cm_eq_cm(gauge_field + ggi(get_index(it, ix, iy, iz, T, L), mu), U1);
-						}
-					}
-				}
-			}
-		}
-
+		do_sweep(gauge_field, T, L, beta, bound_ts);
 		cout << "  ... sweep " << MC_sweep_i << " - end ...\n";
 
 		P = Average_Plaquette(gauge_field, T, L);
